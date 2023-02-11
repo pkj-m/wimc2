b88db0229b56f78a8682e7ca2cd05c1f63a2ab37
Pankaj Mishra <pankajmishra1511@gmail.com>
2023-02-12 04:14:48 +0530

    Better logging

--- mongo/search_results.go
+++ mongo/search_results.go
@@ -15,44 +15,56 @@ import (
 // SaveSearchResults accepts a list of YouTube search results and saves it in a mongo collection tagged with today's date
 func SaveSearchResults(cfg *config.AppConfig, logger *zap.Logger, results []*youtube.SearchResult) error {
 	currentTime := time.Now()
-	today := currentTime.Format("09-07-2017")
+	today := currentTime.Format("01-02-2006")
 
 	collectionName := fmt.Sprintf("%s-%s", cfg.Mongo.CollectionPrefix, today)
+	newLogger := logger.With(
+		zap.String("callSiteTag", "mongo::SaveSearchResults"),
+		zap.String("collection", collectionName),
+		zap.Int("input count", len(results)))
+	newLogger.Info("called")
+
 	err := Client.Database(cfg.Mongo.Database).CreateCollection(context.TODO(), collectionName)
 	if err != nil {
-		logger.Error("failed to create mongo collection", zap.Error(err), zap.String("collection", collectionName))
+		newLogger.Error("failed to create mongo collection", zap.Error(err))
 		return err
 	}
 
-	resultsInterface := convertToInterface(results)
+	resultsInterface := convertToInterface(newLogger, results)
 	res, err := Client.Database(cfg.Mongo.Database).Collection(collectionName).InsertMany(context.TODO(), resultsInterface)
 	if err != nil {
-		logger.Error("failed to save results in collection", zap.Error(err))
+		newLogger.Error("failed to save results in collection", zap.Error(err))
 		return err
 	}
-	logger.Info("successfully saved search results in mongo collection", zap.Int("count", len(res.InsertedIDs)))
+	newLogger.Info("success", zap.Int("saved count", len(res.InsertedIDs)))
 	return nil
 }
 
 // FetchSearchResults accepts a date and returns the list of search results that were received that day from YouTube
 func FetchSearchResults(cfg *config.AppConfig, logger *zap.Logger, target *time.Time) ([]*youtube.SearchResult, error) {
+	targetDate := target.Format("01-02-2006")
+	collectionName := fmt.Sprintf("%s-%s", cfg.Mongo.CollectionPrefix, targetDate)
+
+	newLogger := logger.With(
+		zap.String("callSiteTag", "mongo::FetchSearchResults"),
+		zap.String("collection", collectionName),
+		zap.String("target time", target.String()))
+	newLogger.Info("called")
+
 	if target == nil {
-		logger.Error("nil target passed")
+		newLogger.Error("nil target passed")
 		return nil, errors.New("invalid argument received")
 	}
 
-	targetDate := target.Format("09-07-2017")
-	collectionName := fmt.Sprintf("%s-%s", cfg.Mongo.CollectionPrefix, targetDate)
-
 	coll := Client.Database(cfg.Mongo.Database).Collection(collectionName)
 	if coll == nil {
-		logger.Error("could not find mongo collection", zap.String("collection", collectionName))
+		newLogger.Error("failed to fetch collection")
 		return []*youtube.SearchResult{}, nil
 	}
 
 	cur, err := Client.Database(cfg.Mongo.Database).Collection(collectionName).Find(context.TODO(), bson.D{})
 	if err != nil {
-		logger.Error("error while fetching documents from collection", zap.String("collection", collectionName))
+		newLogger.Error("failed to fetch documents")
 		return nil, err
 	}
 
@@ -61,26 +73,28 @@ func FetchSearchResults(cfg *config.AppConfig, logger *zap.Logger, target *time.
 		var result youtube.SearchResult
 		err := cur.Decode(&result)
 		if err != nil {
-			logger.Error("failed to decode mongo response into searchResult", zap.Error(err), zap.Any("result", cur.Current))
+			newLogger.Error("failed to decode response into searchResult", zap.Error(err), zap.Any("result", cur.Current))
 			continue
 		}
 		results = append(results, &result)
 	}
 
+	newLogger.Info("success", zap.Int("count", len(results)))
 	return results, nil
 }
 
-func convertToInterface(results []*youtube.SearchResult) []interface{} {
+func convertToInterface(logger *zap.Logger, results []*youtube.SearchResult) []interface{} {
 	interfaces := make([]interface{}, 0)
 	for _, result := range results {
 		if result == nil {
+			logger.Error("nil result pointer in results list")
 			continue
 		}
 		var inter interface{}
 		txt, _ := json.Marshal(*result)
-		err := json.Unmarshal(txt, inter)
+		err := json.Unmarshal(txt, &inter)
 		if err != nil {
-			// handle error
+			logger.Error("failed to unmarshal search result", zap.Any("encoded", string(txt)))
 			continue
 		}
 		interfaces = append(interfaces, inter)
